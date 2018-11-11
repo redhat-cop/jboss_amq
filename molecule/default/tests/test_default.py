9c8748527fa01180c0722928f5602d09569b8bb3
Andrew Block <andy.block@gmail.com>
2018-11-11 23:29:19 +0800

    Support for molecule and travis enhancements

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,38 @@
 import os
 
 import testinfra.utils.ansible_runner
+import pytest
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('pkg', [
+  'unzip',
+  'java-1.8.0-openjdk-devel',
+  'python2-pip'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('service', [
+  'jboss-amq',
+])
+def test_systemd(host, service):
+    systemd_service = host.service(service)
+
+    assert systemd_service.is_enabled
+    assert systemd_service.is_running
+
+
+@pytest.mark.parametrize('directory', [
+  '/opt/jboss/amq/brokers',
+])
+def test_host_directories(host, directory):
+    host_directory = host.file(directory)
+
+    assert host_directory.exists
+    assert host_directory.is_directory
